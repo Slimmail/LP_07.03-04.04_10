e5d03cefdf58860ec52d210fc32156eac32e5776
AlekseiBelorukov <slimmail2011@gmail.com>
2023-03-07 14:38:39 +0300

    Сделала решение Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -7,3 +7,17 @@
 // а=5; Ь = -> mах = 7
 // а = 2 Ь = 10 -> mах = 10
 // а = -9 b = -3 -> mах = -3
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) 
+{
+  Console.WriteLine($"Первое число {num1} больше, чем второе число {num2}");
+}
+else 
+{
+  Console.WriteLine($"Второе число {num2} больше, чем первое число {num1}");
+}
