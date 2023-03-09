b468ba507c6c1041a2b88dee6d45dacae912ff19
AlekseiBelorukov <slimmail2011@gmail.com>
2023-03-09 16:47:25 +0300

    Сделала решение Task08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -2,3 +2,15 @@
 
 //  5 -> 2, 4
 //  8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите натуральное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= num)
+{
+  if (count % 2 == 0)
+  {
+   Console.Write(($"{count} ")); 
+  }
+  count++;  
+}
