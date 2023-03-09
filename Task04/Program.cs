b78818cd979af90dc1d6f009ea4ae8667cf0dcf7
AlekseiBelorukov <slimmail2011@gmail.com>
2023-03-09 16:28:09 +0300

    Сделала решение Task04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -4,3 +4,25 @@
 // 2,3,7-> 7
 //  44, 5, 78 -> 78
 // 22,3, 9 -> 22
+
+
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int numMax = num1;
+if (num1 < num2)
+{
+  numMax = num2;
+}
+if (numMax < num3)
+{
+  numMax = num3;
+}
+Console.WriteLine($"Максимальное число {numMax}");
+
+
