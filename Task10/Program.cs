99d6352285ec4e9e7bb3c9706dd6e8047dae6ad5
AlekseiBelorukov <slimmail2011@gmail.com>
2023-03-12 16:51:42 +0300

    Решили Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = GetTwoDigitPosition(number);
+Console.WriteLine(result);
+
+int GetTwoDigitPosition(int num)
+{
+  int twoDigitPosition = num / 10 % 10;
+  return twoDigitPosition;
+}
+
