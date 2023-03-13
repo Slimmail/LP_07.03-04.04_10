c5cc1eb835706047b26a3a378fe4e832024ad619
AlekseiBelorukov <slimmail2011@gmail.com>
2023-03-13 20:04:01 +0300

    Улучшил вторым вариантом решения Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -8,41 +8,70 @@ Console.Clear();
 Console.WriteLine("Введите номер дня недели: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int day = DayDetector(num);
-// Console.WriteLine(day);
+string day = DayDetector(num);
+Console.WriteLine(day);
 
-if (day == 1)
-{ 
-  Console.WriteLine($"{num} -> нет"); 
-}
-else if (day == 2)
+string DayDetector(int x)
 {
-  Console.WriteLine($"{num} -> да");
-}
-else if (day == 3)
-{
-  Console.WriteLine($"{num} -> такого дня не существует");
-}
-
-
-int DayDetector(int x)
-{
-  int d = 0;
+  string d;
   if (x == 1 ^ x == 2 ^ x == 3 ^ x == 4 ^ x == 5)
   {
-    d = 1;
+    d = ($"{num} -> нет");
   }
 
   else if (x == 6 ^ x == 7)
   {
-    d = 2;
+    d = ($"{num} -> да");
   }
 
   else
   {
-    d = 3;
+    d = ($"{num} -> такого дня не существует");
   }
 
   return d;
 
 }
+
+// Второй рабочий вариант:
+
+// Console.Clear();
+// Console.WriteLine("Введите номер дня недели: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int day = DayDetector(num);
+
+// if (day == 1)
+// { 
+//   Console.WriteLine($"{num} -> нет"); 
+// }
+// else if (day == 2)
+// {
+//   Console.WriteLine($"{num} -> да");
+// }
+// else if (day == 3)
+// {
+//   Console.WriteLine($"{num} -> такого дня не существует");
+// }
+
+// int DayDetector(int x)
+// {
+//   int d = 0;
+//   if (x == 1 ^ x == 2 ^ x == 3 ^ x == 4 ^ x == 5)
+//   {
+//     d = 1;
+//   }
+
+//   else if (x == 6 ^ x == 7)
+//   {
+//     d = 2;
+//   }
+
+//   else
+//   {
+//     d = 3;
+//   }
+
+//   return d;
+
+// }
