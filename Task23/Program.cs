dd3143cdc87eba3fb81adaff0a369f1396adf134
AlekseiBelorukov <slimmail2011@gmail.com>
2023-03-14 12:49:16 +0300

    Улучшил код и вписал условия задачи по Task23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,4 +1,11 @@
-﻿
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
 
 Console.Clear();
 Console.Write("Введите натуральное число: ");
@@ -12,7 +19,7 @@ void TableCube(int number)
   while (count <= number)
   {
     int cube = count * count * count;
-    Console.WriteLine($"{count, 2} -> {cube, 3}");
+    Console.WriteLine($"{count, 2} | {cube}");
     count++;
   }
 
