9ac3b51f5f09461aad09b5ee84a0ff90b8cc4d21
AlekseiBelorukov <slimmail2011@gmail.com>
2023-03-14 12:39:27 +0300

    Сделал Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Console.Clear();
+Console.Write("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+TableCube(num);
+
+void TableCube(int number)
+{
+  int count = 1;
+  while (count <= number)
+  {
+    int cube = count * count * count;
+    Console.WriteLine($"{count, 2} -> {cube, 3}");
+    count++;
+  }
+
+}
