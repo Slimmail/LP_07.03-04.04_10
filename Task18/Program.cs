a3fc982d151b8d2af2dc4b74ed8b9e4aec70d0a0
AlekseiBelorukov <slimmail2011@gmail.com>
2023-03-14 11:04:37 +0300

    сделал Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+Console.Clear();
+Console.Write("Введите номер четверти: ");
+int quater = Convert.ToInt32(Console.ReadLine());
+
+string result = GetCoordinate(quater);
+Console.WriteLine(result);
+
+string GetCoordinate(int x)
+{
+  if(x == 1) return "Диапазон 1 четверти: x > 0, y > 0";
+  if(x == 2) return "Диапазон 2 четверти: x < 0, y > 0";
+  if(x == 3) return "Диапазон 3 четверти: x < 0, y < 0";
+  if(x == 4) return "Диапазон 4 четверти: x > 0, y < 0";
+  return "Некорректный номер четверти";
+}
+
