7054254d4d5a7ec519b55d46d746ef88e7df6a72
AlekseiBelorukov <slimmail2011@gmail.com>
2023-03-29 18:27:23 +0300

    Ready Task45

---
+++ Task45/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+
+int[] ArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] CopyArray(int[] arr)
+{
+    int[] copyArray = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        copyArray[i] = arr[i];
+
+    }
+    return copyArray;
+}
+
+int[] array = ArrayRndInt(7, 0, 30);
+PrintArray(array);
+
+Console.WriteLine();
+int[] copyArr = CopyArray(array);
+// copyArr[0] = 32; была бы ссылка был бы такой же результат
+PrintArray(copyArr);
+
