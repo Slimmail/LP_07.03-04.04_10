61e66f781610bab92aaa8efc20d9d8562418e2ba
AlekseiBelorukov <slimmail2011@gmail.com>
2023-04-07 00:02:17 +0300

    Ready Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая 
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+int[,] arr2D = CreateMatrixRndInt(3,4,0,10);
+PrintMatrix(arr2D);
+Console.WriteLine();
+ArrSortString(arr2D);
+PrintMatrix(arr2D);
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{                          // 0       1
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix) // метод печати 2D массива целых чисел
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+void ArrSortString(int[,] arr) // метод сортировки строк 2Д матрице
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      for (int k = 0; k < arr.GetLength(1) - 1; k++)
+      {
+        if (arr[i, k] < arr[i, k + 1])
+        {
+          int temp = arr[i, k + 1];
+          arr[i, k + 1] = arr[i, k];
+          arr[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+
