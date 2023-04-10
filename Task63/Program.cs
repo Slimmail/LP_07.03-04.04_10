5b7a48709a40cbd717e99a1b602971865c0d80f6
AlekseiBelorukov <slimmail2011@gmail.com>
2023-04-11 01:58:59 +0300

    Ready Task63

---
+++ Task63/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.WriteLine($"Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NuturalNumbers(number);
+
+void NuturalNumbers(int num)
+{
+    if(num == 0) return;
+    NuturalNumbers (num - 1);
+    Console.Write($"{num} ");
+ }
+
+
+
+
+
+
+
